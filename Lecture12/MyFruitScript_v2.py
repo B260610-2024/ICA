973f4034401ee618a6f0343af9b9bf358d660580
B260610-2024 <s2671467@ed.ac.uk>
2024-12-15 14:51:50 +0000

    Script from the second BPSM pyhton lecture

---
+++ Lecture12/MyFruitScript_v2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#scripts to cpmare friut
+#version2
+import os,sys
+# Allow fo two fruit
+first_fruit = input("What is the first fruit?\n\t")
+second_fruit = input("What is the second fruit?\n\t")
+print(first_fruit.upper()+" are just as nice as "+second_fruit.upper())
+
